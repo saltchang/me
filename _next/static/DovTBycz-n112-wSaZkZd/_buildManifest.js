42cfdf5ed53f6a0864a0731dd74685673a2c7888
saltchang <saltchang.tw@gmail.com>
2022-03-24 09:30:53 +0000

    Deploying to gh-pages from @ saltchang/me@ae41749365b18c3e921c29306ef9ab974a374eec 🚀

---
+++ _next/static/DovTBycz-n112-wSaZkZd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-5a5657d726d85af7.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
